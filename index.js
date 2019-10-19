e44d29e2e3af3eec581ada3f0e499e1a072dabe2
alomako <lomako.artur@gmail.com>
2019-10-19 23:13:36 +0200

    Move changeParent function to separate file

--- index.js
+++ index.js
@@ -9,31 +9,6 @@ const show = (...elements) =>
 const getContainer = (layerNumber, className) =>
   getByClass(`layer-${layerNumber}__${className}-container`);
 
-const changeParent = (element, newParent, afterTransitionCallback) => {
-  const getRect = () => element.getBoundingClientRect();
-  const withReflow = fn => {
-    fn();
-    element.innerText;
-  };
-
-  const oldRect = getRect();
-  withReflow(() => newParent.appendChild(element));
-  const newRect = getRect();
-
-  element.style.transform = `translateX(${oldRect.x -
-    newRect.x}px) translateY(${oldRect.y - newRect.y}px)`;
-
-  anime({
-    targets: element,
-    translateX: 0,
-    translateY: 0,
-    width: newRect.width,
-    duration: 400,
-    easing: "easeInOutQuad",
-    complete: afterTransitionCallback
-  });
-};
-
 const hideLayer = layer => {
   layer.style.visibility = "hidden";
   layer.style["z-index"] = 0;
